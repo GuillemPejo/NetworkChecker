ff8c618c856195e5c4641cad047d8835dd3ef92d
guillem <guillempejo@gmail.com>
2021-02-01 17:29:24 +0100

    Add functions

--- app/src/main/java/me/guillem/networkchecker/MainActivity.java
+++ app/src/main/java/me/guillem/networkchecker/MainActivity.java
@@ -2,15 +2,34 @@ package me.guillem.networkchecker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
 
+public class MainActivity extends AppCompatActivity implements ConnectionReceiver.ReceiverListener {
+
+    Button btchecker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btchecker = findViewById(R.id.btChecker);
+        checkConnection();
+
+        btchecker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkConnection();
+            }
+        });
     }
 
     private void checkConnection(){
@@ -18,6 +37,50 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         registerReceiver(new ConnectionReceiver(),intentFilter);
 
+        ConnectionReceiver.listener = this;
+
+        ConnectivityManager manager = (ConnectivityManager)
+                getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isConnected = networkInfo != null && networkInfo.isConnectedOrConnecting();
+        
+        showSnack(isConnected);
+    }
+
+    private void showSnack(boolean isConnected) {
+        int color;
+        String msg;
+        if (isConnected){
+            msg = "Connectes to internet";
+            color = Color.WHITE;
+        }else {
+            msg = "Not connected to internet";
+            color = Color.RED;
+        }
+
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.),msg, Snackbar.LENGTH_LONG);
+        View v = snackbar.getView();
+        TextView sntv = v.findViewById(R.id.snackbar_text);
+        sntv.setTextColor(color);
+        snackbar.show();
+    }
+
+    @Override
+    public void onNetworkChange(boolean isConnected) {
+        showSnack(isConnected);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkConnection();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        checkConnection();
 
     }
 }
